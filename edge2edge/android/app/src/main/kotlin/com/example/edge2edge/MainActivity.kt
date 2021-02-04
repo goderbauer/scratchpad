fb5c97d3ecadf6c48bff20b5a8d83f8b19226070
Michael Goderbauer <goderbauer@google.com>
2021-02-04 09:37:33 -0800

    use native API

--- edge2edge/android/app/src/main/kotlin/com/example/edge2edge/MainActivity.kt
+++ edge2edge/android/app/src/main/kotlin/com/example/edge2edge/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.edge2edge
 
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onPostResume() {
+      super.onPostResume()
+      WindowCompat.setDecorFitsSystemWindows(window, false)
+      window.navigationBarColor = 0
+    }
 }
--- edge2edge/lib/main.dart
+++ edge2edge/lib/main.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.transparent,
-  ));
-  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
   runApp(MyApp());
 }
 
