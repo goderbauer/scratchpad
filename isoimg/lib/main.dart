4e2c19ef549d12921cb26c559211000face67a1a
Michael Goderbauer <goderbauer@google.com>
2022-03-08 13:21:38 -0800

    ++

--- isoimg/lib/main.dart
+++ isoimg/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:image/image.dart' as image;
 import 'package:http/http.dart' as http;
@@ -19,11 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Processing Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Image Processing Demo'),
     );
   }
 }
