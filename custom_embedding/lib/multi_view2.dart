0bafc906b47d86f3734e4cc48008188b526e1ff5
Michael Goderbauer <goderbauer@google.com>
2022-08-17 14:52:51 -0700

    Works with sideviews

--- custom_embedding/lib/multi_view2.dart
+++ custom_embedding/lib/multi_view2.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  // TODO(goderbauer): Better error when accidentally uing runApp.
+  // TODO(goderbauer): Better error when accidentally using runApp.
   runPlainApp(const MyApp());
 }
 
@@ -16,16 +16,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Color _background = Colors.white;
+
   late List<Widget> _views = [
     View(
       view: PlatformDispatcher.instance.views.first,
       child: MainScreen(
-        onCreateView: _onCreateView,
+        onCreateView: _handleCreateView,
+        onChangeBackground: _handleBackgroundChange,
       ),
     ),
   ];
 
-  Future<void> _onCreateView() async {
+  Future<void> _handleCreateView() async {
     final int viewId = await _ViewManager.createView();
     if (!mounted) {
       return;
@@ -35,32 +38,114 @@ class _MyAppState extends State<MyApp> {
         View(
           view: PlatformDispatcher.instance.views.firstWhere((FlutterView view) => view.viewId == viewId),
           child: const TopLevelView(),
-        )
+        ),
       );
+    });
+  }
+  void _handleBackgroundChange() {
+    setState(() {
+      _background = _background == Colors.white ? Colors.grey : Colors.white;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return ViewStages(
-      children: _views,
+    return BackgroundColor(
+      color: _background,
+      child: ViewStages(
+        children: _views,
+      ),
     );
   }
 }
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key, required this.onCreateView});
+// TODO(window): hot reload/restart.
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key, required this.onCreateView, required this.onChangeBackground});
 
   final VoidCallback onCreateView;
+  final VoidCallback onChangeBackground;
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<Widget> _sideViews = const [];
+
+  Future<void> _createSideView() async {
+    final int viewId = await _ViewManager.createView();
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      _sideViews = _sideViews.toList()..add(
+        View(
+          view: PlatformDispatcher.instance.views.firstWhere((FlutterView view) => view.viewId == viewId),
+          child: const SideView(),
+        ),
+      );
+    });
+  }
+
+  Color _sideBackground = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("MAIN VIEW ${View.of(context).viewId}")),
-        floatingActionButton: FloatingActionButton(
-          onPressed: onCreateView,
-        ),
+        body: Container(
+          color: BackgroundColor.of(context),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    OutlinedButton(
+                      onPressed: widget.onCreateView,
+                      child: const Text('top-level view'),
+                    ),
+                    const SizedBox(width: 8),
+                    // TODO(window): This widget is throwing.
+                    BackgroundColor(
+                      color: _sideBackground,
+                      child: ViewSideStages(
+                        sideViews: _sideViews,
+                        child: OutlinedButton(
+                          onPressed: _createSideView,
+                          child: const Text('side view'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    OutlinedButton(
+                      onPressed: widget.onChangeBackground,
+                      child: const Text('change top color'),
+                    ),
+                    const SizedBox(width: 8),
+                    OutlinedButton(
+                      onPressed: () {
+                        setState(() {
+                          _sideBackground = _sideBackground == Colors.white ? Colors.lightGreen : Colors.white;
+                        });
+                      },
+                      child: const Text('change side color'),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ),
+        )
       ),
     );
   }
@@ -74,10 +159,56 @@ class TopLevelView extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Top Level View ${View.of(context).viewId}")),
+        body: SizedBox.expand(
+          child: ColoredBox(
+            color: BackgroundColor.of(context),
+            child: const Center(
+              child: Text("Hello"),
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+class SideView extends StatelessWidget {
+  const SideView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Side View ${View.of(context).viewId}")),
+          body: SizedBox.expand(
+            child: ColoredBox(
+              color: BackgroundColor.of(context),
+              child: const Center(
+                child: Text("Hello"),
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
+
+class BackgroundColor extends InheritedWidget {
+  const BackgroundColor({super.key, required this.color, required super.child});
+
+  final Color color;
+
+  @override
+  bool updateShouldNotify(BackgroundColor oldWidget) {
+    return color != oldWidget.color;
+  }
+
+  static Color of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<BackgroundColor>()!.color;
+  }
+}
+
+
 
 
 class _ViewManager {
