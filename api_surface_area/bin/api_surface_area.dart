00870a9f2fec0c013a07193d0b6c2d7c8d1a1092
Michael Goderbauer <goderbauer@google.com>
2026-02-23 10:45:26 +0100

    add api_surface_area tool

---
+++ api_surface_area/bin/api_surface_area.dart
@@ -0,0 +1,5 @@
+import 'package:api_surface_area/api_surface_area.dart' as api_surface_area;
+
+void main(List<String> arguments) {
+  print('Hello world: ${api_surface_area.calculate()}!');
+}
---
+++ api_surface_area/test/api_surface_area_test.dart
@@ -0,0 +1,8 @@
+import 'package:api_surface_area/api_surface_area.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
